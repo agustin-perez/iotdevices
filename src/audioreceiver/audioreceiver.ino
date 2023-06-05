009c94284627ea36f964bcc49a257e6a97b063b3
agustin-perez <agperez.prieto@protonmail.com>
2023-06-05 18:56:26 -0300

    Audioreceiver almost done

--- src/audioreceiver/audioreceiver.ino
+++ src/audioreceiver/audioreceiver.ino
@@ -4,30 +4,52 @@
 #include <WebServer.h>
 #include <AutoConnect.h>
 #include <LittleFS.h>
+#include <IRsend.h>
+#include <iostream>
+#include <string>
+#include <sstream>
 
-//DEFINITIONS
-#define RL1 16    //D0
-#define RL2 4     //D2
-#define TS1 15    //D8
-#define TS2 13    //D7
-#define BZ1 34     
-#define BZ1SET 1500
-#define BZ1SETLENGTH 1200
-#define BZ1TSON 1800
-#define BZ1TSONLENGTH 300
-#define BZ1TSOFF 1500
-#define BZ1TSOFFLENGTH 300
+//HARDWARE -------------------
+//BOARD
+#define BZ1 14
+//SOCKET 0
+#define IRLED 13
+//SOCKET 1
+#define OPTOBAND    5  //S1-B
+#define OPTOFMMODE  18 //S1-G
+#define OPTOMEMORY  19 //S1-O
+#define OPTOTUNINGD 21 //S1-Y
+#define OPTOTUNINGU 22 //S1-W
+#define OPTODTUNING 23 //S1-R
+//SOCKET 2
+#define OPTOSPKA    15 //S2-B     
+#define OPTOSPKB    2  //S2-G
+#define OPTOVCR1    0  //S2-O
+#define OPTOCD      4  //S2-Y
+#define OPTOTUNER   16 //S2-W
+#define OPTOPHONO   17 //S2-R
+//SOCKET 3
+#define LEDB        26 //S3-B
+#define LEDG        25 //S3-G
+#define LEDR        33 //S3-O
+#define OPTOPOWER   32 //S3-Y
+#define POWERSENS   12 //S3-W
+//CODE -----------------------
 #define FORMAT_ON_FAIL
-#define STAMqttClientID "Technics SA-GX170"
+#define CLIENTID "Audio Receiver"
+#define PRONTOLENGTH 104
 
-enum Color { standby, error, onAction, onBoot, onEspError };
-const char* mqttClientID = STAMqttClientID;
+enum Buzzer {set, on, off, action};
+enum Opto {power, band, fmmode, memory, tuningd, tuningu, dtuning, spka, spkb, vcr1, cd, tuner, phono};
+enum Color {standby, error, onAction, onBoot, onEspError};
+
+const char* mqttClientID = CLIENTID;
 const char* paramFile = "/param.json";
 const char* urlMqttHome = "/";
 const char* urlSettings = "/settings";
 const char* urlSettingsSave = "/save";
-static const char homePage[] PROGMEM = R"({"title": "Technics SA-GX170", "uri": "/", "menu": true, "element": [
-      {"name": "header", "type": "ACElement", "value": "<h2 style='text-align:center;color:#2f4f4f;margin-top:10px;margin-bottom:10px'>Technics SA-GX170 setup page</h2>"},
+static const char homePage[] PROGMEM = R"({"title": "Audio Receiver", "uri": "/", "menu": true, "element": [
+      {"name": "header", "type": "ACElement", "value": "<h2 style='text-align:center;color:#2f4f4f;margin-top:10px;margin-bottom:10px'>Audio Receiver setup page</h2>"},
       { "name": "content", "type": "ACText", "value": "Powered by <a href=https://github.com/Hieromon/AutoConnect>AutoConnect</a>"},
       { "name": "content2", "type": "ACText", "value": "<br>Part of the project <a href=https://github.com/agustin-perez/iotdevices>IoTDevices</a><br>Agustín Pérez"}]
 })";
@@ -39,125 +61,203 @@ static const char settingsPage[] PROGMEM = R"({"title": "Settings", "uri": "/set
       {"name": "mqttUser", "type": "ACInput", "label": "User", "global": true},
       {"name": "mqttPass", "type": "ACInput", "label": "Password", "apply": "password", "global": true},
       {"name": "caption2", "type": "ACText", "value": "Relay powered on at boot<br>Useful for always-on appliances", "posterior": "par"},
-      {"name": "rl1state", "type": "ACCheckbox", "label": "Relay 1", "checked": "false", "global": true},
-      {"name": "rl2state", "type": "ACCheckbox", "label": "Relay 2", "checked": "false", "global": true},
+      {"name": "buzzer", "type": "ACCheckbox", "label": "Disable buzzer", "checked": "false", "global": true},
+      {"name": "voldown", "type": "ACCheckbox", "label": "Volume down on shutdown", "checked": "false", "global": true},
+      {"name": "volsteps", "type": "ACRange", "label": "Volume steps", "min": "1", "max": "10", "global": true},
       {"name": "save", "type": "ACSubmit", "value": "Save and connect", "uri": "/save"},
       {"name": "discard", "type": "ACSubmit", "value": "Discard", "uri": "/"}]
 })";
-static const char saveSettingsPage[] PROGMEM = R"({"title": "Save", "uri": "/save", "menu": true, "element": [
+static const char saveSettingsFilePage[] PROGMEM = R"({"title": "Save", "uri": "/save", "menu": true, "element": [
       { "name": "caption", "type": "ACText", "value": "<h2>Settings saved!</h2>",  "style": "text-align:center;color:#2f4f4f;padding:10px;" }]
 })";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 WebServer server;
+IRsend irsend(IRLED);
 AutoConnect portal(server);
 AutoConnectConfig config;
 AutoConnectAux homePageObj;
 AutoConnectAux settingsPageObj;
-AutoConnectAux saveSettingsPageObj;
+AutoConnectAux saveSettingsFilePageObj;
 fs::LittleFSFS& FlashFS = LittleFS;
+uint16_t chipIDRaw = 0;
+String chipID;
 String strTopic;
 String strPayload;
 String mqttServer;
 String mqttUser;
 String mqttPass;
-uint32_t chipIDRaw = 0;
-String chipID;
-bool prevTS1State;
-bool prevTS2State;
-bool ts1State;
-bool ts2State;
-bool rl1state = false;
-bool rl2state = false;
+bool buzzer;
+bool voldown;
+uint8_t volsteps; 
+bool powerState;
 unsigned long previousMillis = 0;
 const long interval = 500;
 
-void rl1Toggle() {
-  digitalWrite(RL1, !digitalRead(RL1));
-  if (digitalRead(RL1) == HIGH) {
-    client.publish(("stat/wallsocket/" + chipID + "/RL1").c_str(), "ON");
-    tone(BZ1, BZ1TSON, BZ1TSONLENGTH);
-  } else {
-    client.publish(("stat/wallsocket/" + chipID + "/RL1").c_str(), "OFF");
-    tone(BZ1, BZ1TSOFF, BZ1TSOFFLENGTH);
+void beep(Buzzer buzzer){
+  switch (buzzer){
+    case set: tone(BZ1, 3500, 1200);
+    break;
+    case on: tone(BZ1, 3000, 300);
+    break;
+    case off: tone(BZ1, 1500, 300);
+    break;
+    case action: tone(BZ1, 2000, 50);
+    break;
   }
 }
 
-void rl2Toggle() {
-  digitalWrite(RL2, !digitalRead(RL2));
-  if (digitalRead(RL2) == HIGH) {
-    client.publish(("stat/wallsocket/" + chipID + "/RL2").c_str(), "ON");
-    tone(BZ1, BZ1TSON, BZ1TSONLENGTH);
-  } else {
-    client.publish(("stat/wallsocket/" + chipID + "/RL2").c_str(), "OFF");
-    tone(BZ1, BZ1TSOFF, BZ1TSOFFLENGTH);
+void sendOpto(Opto opto){
+    switch (buzzer){
+    case power:      
+      digitalWrite(OPTOPOWER, HIGH);
+      delay(40);
+      digitalWrite(OPTOPOWER, LOW);
+    break;
+    case band:
+      digitalWrite(OPTOBAND, HIGH);
+      delay(40);
+      digitalWrite(OPTOBAND, LOW);
+    break;
+    case fmmode:
+      digitalWrite(OPTOFMMODE, HIGH);
+      delay(40);
+      digitalWrite(OPTOFMMODE, LOW);
+    break;
+    case memory:
+      digitalWrite(OPTOMEMORY, HIGH);
+      delay(40);
+      digitalWrite(OPTOMEMORY, LOW);
+    break;
+    case tuningd:
+      digitalWrite(OPTOTUNINGD, HIGH);
+      delay(40);
+      digitalWrite(OPTOTUNINGD, LOW);
+    break;
+    case tuningu:
+      digitalWrite(OPTOTUNINGU, HIGH);
+      delay(40);
+      digitalWrite(OPTOTUNINGU, LOW);
+    break;
+    case dtuning:
+      digitalWrite(OPTODTUNING, HIGH);
+      delay(40);
+      digitalWrite(OPTODTUNING, LOW);
+    break;
+    case spka:
+      digitalWrite(OPTOSPKA, HIGH);
+      delay(40);
+      digitalWrite(OPTOSPKA, LOW);
+    break;
+    case spkb:
+      digitalWrite(OPTOSPKB, HIGH);
+      delay(40);
+      digitalWrite(OPTOSPKB, LOW);
+    break;
+    case vcr1:
+      digitalWrite(OPTOVCR1, HIGH);
+      delay(40);
+      digitalWrite(OPTOVCR1, LOW);
+    break;
+    case cd:
+      digitalWrite(OPTOCD, HIGH);
+      delay(40);
+      digitalWrite(OPTOCD, LOW);
+    break;
+    case tuner:
+      digitalWrite(OPTOTUNER, HIGH);
+      delay(40);
+      digitalWrite(OPTOTUNER, LOW);
+    break;
+    case phono:
+      digitalWrite(OPTOPHONO, HIGH);
+      delay(40);
+      digitalWrite(OPTOPHONO, LOW);
+    break;
   }
+}
+
+void sendProntoStr(char* cmnd){
+  std::string cmndStr(cmnd);
+  uint16_t cmndArr[PRONTOLENGTH];
+  uint16_t tempValue;
+  uint16_t index = 0;
+  cmndStr.erase(std::remove(cmndStr.begin(), cmndStr.end(), ' '), cmndStr.end());
+  std::istringstream iss(cmndStr);
+  while (iss >> std::hex >> tempValue) {
+    cmndArr[index] = tempValue;
+    index++;
+  }
+  irsend.sendPronto(cmndArr, PRONTOLENGTH);
+}
+
+void setLED(){
+
 }
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
   payload[length] = '\0';
   strTopic = String((char*)topic);
-
-  if (strTopic == "cmnd/wallsocket/" + chipID + "/RL1") {
-    if (payload[0] == '1') {
-      digitalWrite(RL1, HIGH);
-      client.publish(("stat/wallsocket/" + chipID + "/RL1").c_str(), "ON");
-      tone(BZ1, BZ1TSON, BZ1TSONLENGTH);
-    }
-    if (payload[0] == '0') {
-      digitalWrite(RL1, LOW);
-      client.publish(("stat/wallsocket/" + chipID + "/RL1").c_str(), "OFF");
-      tone(BZ1, BZ1TSOFF, BZ1TSOFFLENGTH);
-    }
+  if (strTopic == "cmnd/audioreceiver/" + chipID + "/BTN") {
+    strPayload = String((char*)payload);
+    if(strPayload == "POWER") sendOpto(Opto::power);
+    else if(strPayload == "BAND") sendOpto(Opto::band);
+    else if(strPayload == "FMMODE") sendOpto(Opto::fmmode);
+    else if(strPayload == "MEMORY") sendOpto(Opto::memory);
+    else if(strPayload == "TUNINGD") sendOpto(Opto::tuningd);
+    else if(strPayload == "TUNINGU") sendOpto(Opto::tuningu);
+    else if(strPayload == "DTUNING") sendOpto(Opto::dtuning);
+    else if(strPayload == "SPKA") sendOpto(Opto::spka);
+    else if(strPayload == "SPKB") sendOpto(Opto::spkb);
+    else if(strPayload == "VCR1") sendOpto(Opto::vcr1);
+    else if(strPayload == "CD") sendOpto(Opto::cd);
+    else if(strPayload == "TUNER") sendOpto(Opto::tuner);
+    else if(strPayload == "PHONO") sendOpto(Opto::phono);
   }
+  if (strTopic == "cmnd/audioreceiver/" + chipID + "/IR") {
+    sendProntoStr((char*)payload);
+  }
+  if (strTopic == "cmnd/audioreceiver/" + chipID + "/LED") {
 
-  if (strTopic == "cmnd/wallsocket/" + chipID + "/RL2") {
-    if (payload[0] == '1') {
-      digitalWrite(RL2, HIGH);
-      client.publish(("stat/wallsocket/" + chipID + "/RL2").c_str(), "ON");
-      tone(BZ1, BZ1TSON, BZ1TSONLENGTH);
-    }
-    if (payload[0] == '0') {
-      digitalWrite(RL2, LOW);
-      client.publish(("stat/wallsocket/" + chipID + "/RL2").c_str(), "OFF");
-      tone(BZ1, BZ1TSOFF, BZ1TSOFFLENGTH);
-    }
   }
 }
 
-String submitSettings(AutoConnectAux& aux, PageArgument& args) {
+String settingsToPage(AutoConnectAux& aux, PageArgument& args) {
   aux[F("mqttServer")].as<AutoConnectInput>().value = mqttServer;
   aux[F("mqttUser")].as<AutoConnectInput>().value = mqttUser;
   aux[F("mqttPass")].as<AutoConnectInput>().value = mqttPass;
-  aux[F("rl1state")].as<AutoConnectInput>().value = rl1state;
-  aux[F("rl2state")].as<AutoConnectInput>().value = rl2state;
+  aux[F("buzzer")].as<AutoConnectInput>().value = buzzer;
+  aux[F("voldown")].as<AutoConnectInput>().value = voldown;
+  aux[F("volsteps")].as<AutoConnectInput>().value = volsteps;
   return String();
 }
 
-String updateSettings(AutoConnectAux& aux) {
+String loadSavedSettings(AutoConnectAux& aux) {
   mqttServer = aux[F("mqttServer")].as<AutoConnectInput>().value;
   mqttUser = aux[F("mqttUser")].as<AutoConnectInput>().value;
   mqttPass = aux[F("mqttPass")].as<AutoConnectInput>().value;
-  AutoConnectCheckbox& rl1stateCheckbox = aux[F("rl1state")].as<AutoConnectCheckbox>();
-  rl1state = rl1stateCheckbox.checked;
-  AutoConnectCheckbox& rl2stateCheckbox = aux[F("rl2state")].as<AutoConnectCheckbox>();
-  rl1state = rl2stateCheckbox.checked;
+  AutoConnectCheckbox& buzzerCheckbox = aux[F("buzzer")].as<AutoConnectCheckbox>();
+  buzzer = buzzerCheckbox.checked;
+  AutoConnectCheckbox& voldownCheckbox = aux[F("voldown")].as<AutoConnectCheckbox>();
+  voldown = voldownCheckbox.checked;
+  AutoConnectRange& volstepsRange = aux[F("volsteps")].as<AutoConnectRange>();
+  volsteps = volstepsRange.step;
   return String();
 }
 
-void loadParams(AutoConnectAux& aux) {
+void loadSettingsFile(AutoConnectAux& aux) {
   File param = FlashFS.open(paramFile, "r");
-  if (param && aux.loadElement(param)) updateSettings(aux);
+  if (param && aux.loadElement(param)) loadSavedSettings(aux);
   param.close();
 }
 
-String saveSettings(AutoConnectAux& aux, PageArgument& args) {
+String saveSettingsFile(AutoConnectAux& aux, PageArgument& args) {
   AutoConnectAux& settings = aux.referer();
-  updateSettings(settings);
+  loadSavedSettings(settings);
   File param = FlashFS.open(paramFile, "w");
   if (param) {
-    settings.saveElement(param, {"mqttServer", "mqttUser", "mqttPass", "rl1state", "rl2state"});
+    settings.saveElement(param, {"mqttServer", "mqttUser", "mqttPass", "buzzer", "voldown", "volsteps"});
     param.close();
   }
   return String();
@@ -168,32 +268,67 @@ void mqttReconnect() {
   client.setCallback(mqttCallback);
   client.setServer(mqttServer.c_str(), 1883);
   if (client.connect(mqttClientID, mqttUser.c_str(), mqttPass.c_str())) {
-    client.subscribe(("avail/wallsocket/" + chipID).c_str());
-    client.subscribe(("cmnd/wallsocket/" + chipID + "/RL1").c_str());
-    client.subscribe(("cmnd/wallsocket/" + chipID + "/RL2").c_str());
-    client.subscribe(("stat/wallsocket/" + chipID + "/RL1").c_str());
-    client.subscribe(("stat/wallsocket/" + chipID + "/RL2").c_str());
-    client.publish(("avail/wallsocket/" + chipID).c_str(), "Online");
+    client.subscribe(("avail/audioreceiver/" + chipID).c_str());
+    client.subscribe(("cmnd/audioreceiver/" + chipID + "/BTN").c_str());
+    client.subscribe(("cmnd/audioreceiver/" + chipID + "/IR").c_str());
+    client.subscribe(("cmnd/audioreceiver/" + chipID + "/LED").c_str());
+    client.subscribe(("stat/audioreceiver/" + chipID + "/POWER").c_str());
+    client.publish(("avail/audioreceiver/" + chipID).c_str(), "ONLINE");
   } else {
     delay(2000);
   }
 }
 
-void setup() {
-  pinMode(RL1, OUTPUT);
-  digitalWrite(RL1, LOW);
-  pinMode(RL2, OUTPUT);
-  digitalWrite(RL2, LOW);
-  pinMode(TS1, INPUT);
-  pinMode(TS2, INPUT);
+void hardwareInit(){
   pinMode(BZ1, OUTPUT);
-  tone(BZ1, BZ1SET, BZ1SETLENGTH);
-  delay(1000);
+  digitalWrite(BZ1, LOW);
+  pinMode(IRLED, OUTPUT);
+  digitalWrite(IRLED, LOW);
+  pinMode(OPTOBAND, OUTPUT);
+  digitalWrite(OPTOBAND, LOW);
+  pinMode(OPTOFMMODE, OUTPUT);
+  digitalWrite(OPTOFMMODE, LOW);
+  pinMode(OPTOMEMORY, OUTPUT);
+  digitalWrite(OPTOMEMORY, LOW);
+  pinMode(OPTOTUNINGD, OUTPUT);
+  digitalWrite(OPTOTUNINGD, LOW);
+  pinMode(OPTOTUNINGU, OUTPUT);
+  digitalWrite(OPTOTUNINGU, LOW);
+  pinMode(OPTODTUNING, OUTPUT);
+  digitalWrite(OPTODTUNING, LOW);
+  pinMode(OPTOSPKA, OUTPUT);
+  digitalWrite(OPTOSPKA, LOW);
+  pinMode(OPTOSPKB, OUTPUT);
+  digitalWrite(OPTOSPKB, LOW);
+  pinMode(OPTOVCR1, OUTPUT);
+  digitalWrite(OPTOVCR1, LOW);
+  pinMode(OPTOCD, OUTPUT);
+  digitalWrite(OPTOCD, LOW);
+  pinMode(OPTOTUNER, OUTPUT);
+  digitalWrite(OPTOTUNER, LOW);
+  pinMode(OPTOPHONO, OUTPUT);
+  digitalWrite(OPTOPHONO, LOW);
+  pinMode(LEDB, OUTPUT);
+  digitalWrite(LEDB, LOW);
+  pinMode(LEDG, OUTPUT);
+  digitalWrite(LEDG, LOW);
+  pinMode(LEDR, OUTPUT);
+  digitalWrite(LEDR, LOW);
+  pinMode(OPTOPOWER, OUTPUT);
+  digitalWrite(OPTOPOWER, LOW);
+  pinMode(POWERSENS, INPUT);
+}
+
+void softwareInit(){
   for (int i = 0; i < 17; i = i + 8) {
     chipIDRaw |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
   }
   chipID = String(chipIDRaw);
   FlashFS.begin(FORMAT_ON_FAIL);
+  irsend.begin();
+}
+
+void autoConnectInit(){
   config.autoReconnect = true;
   config.reconnectInterval = 1;
   config.ota = AC_OTA_BUILTIN;
@@ -201,26 +336,23 @@ void setup() {
   portal.config(config);
   homePageObj.load(homePage);
   settingsPageObj.load(settingsPage);
-  saveSettingsPageObj.load(saveSettingsPage);
-  portal.join({ homePageObj, settingsPageObj, saveSettingsPageObj });
-  portal.on(urlSettings, submitSettings);
-  portal.on(urlSettingsSave, saveSettings);
+  saveSettingsFilePageObj.load(saveSettingsFilePage);
+  portal.join({ homePageObj, settingsPageObj, saveSettingsFilePageObj });
+  portal.on(urlSettings, settingsToPage);
+  portal.on(urlSettingsSave, saveSettingsFile);
   AutoConnectAux& settingsRestore = *portal.aux(urlSettings);
-  loadParams(settingsRestore);
+  loadSettingsFile(settingsRestore);
   portal.begin();
-  if (rl1state) digitalWrite(RL1, HIGH);
-  if (rl2state) digitalWrite(RL2, HIGH);
-  bool prevTS1State = digitalRead(TS1);
-  bool prevTS2State = digitalRead(TS2);
+}
+
+void setup() {
+  hardwareInit();
+  beep(Buzzer::set);
+  autoConnectInit();
 }
 
 void millisLoop() {
-  prevTS1State = ts1State;
-  prevTS2State = ts2State;
-  ts1State = digitalRead(TS1);
-  ts2State = digitalRead(TS2);
-  if (!prevTS1State && ts1State) rl1Toggle();
-  if (!prevTS2State && ts2State) rl2Toggle();
+
 }
 
 void loop() {
